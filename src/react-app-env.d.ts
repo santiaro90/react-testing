75872a6ba27cb7a88ef462749922684e1e236620
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2020-05-16 12:47:54 -0500

    Set up custom matcher and env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_API_URL: string;
+  }
+}
+
+declare namespace jest {
+  // namespace jest {
+  interface Matchers<R> {
+    toHavePendingRequests(): CustomMatcherResult;
+  }
+  // }
+}
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import nock from "nock";
+import { cleanup } from "@testing-library/react";
+
+expect.extend({
+  toHavePendingRequests(this: jest.MatcherUtils, api: nock.Scope) {
+    const pass = !api.isDone();
+
+    return {
+      pass,
+      message: pass
+        ? () => "Expected Nock not to have pending requests"
+        : () => "Expected Nock to have pending requests",
+    };
+  },
+});
+
+afterEach(() => {
+  cleanup();
+  expect(nock).not.toHavePendingRequests();
+});
